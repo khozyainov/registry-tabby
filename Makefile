05d3dc0d492b09080da4f71bbada826f03fe743d
Anton Khozyainov <a.khozyainov@cherryhome.ai>
2023-12-04 21:33:00 +0300

    Add deepseek 6.7b instruct Q5_K_M

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+.EXPORT_ALL_VARIABLES:
+
+GITHUB_USERNAME := $(shell git config --get remote.origin.url | awk -F'/' '{print $$4}') 
+
 all: README.md models.json
 
 README.md: clean models.json
--- scripts/generate_readme.py
+++ scripts/generate_readme.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+import os
 import json
 from jinja2 import Environment, FileSystemLoader
 from datetime import datetime, timedelta
+
+GITHUB_USERNAME = os.getenv("GITHUB_USERNAME", "TabbyML").strip()
 
 env = Environment(loader=FileSystemLoader("."))
 template = env.get_template("meta/README.tmpl.md")
@@ -13,7 +16,7 @@ with open("models.json", "r") as f:
 completion_models = [x for x in models if 'prompt_template' in x]
 chat_models = [x for x in models if 'chat_template' in x]
 
-rendered_template = template.render(completion_models=completion_models, chat_models=chat_models)
+rendered_template = template.render(completion_models=completion_models, chat_models=chat_models, gh_user=GITHUB_USERNAME)
 
 with open("README.md", "w") as f:
     f.write(rendered_template)
